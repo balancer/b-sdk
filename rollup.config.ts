ba2be69ba8f2b47ed9cc9bfd7726a8af141f94f7
Daniel <danielBeets@protonmail.com>
2023-03-02 09:01:42 +0100

    Add rollup config and update build command in the package.json

---
+++ rollup.config.ts
@@ -0,0 +1,64 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
+import dts from 'rollup-plugin-dts';
+import pkg from './package.json';
+
+const external = [...Object.keys(pkg.dependencies)];
+
+export default [
+    {
+        input: 'src/index.ts',
+        output: [
+            {
+                name: 'balancer-js',
+                file: pkg.browser,
+                format: 'umd',
+                sourcemap: true,
+                globals: {
+                    '@ethersproject/abi': 'abi',
+                    '@ethersproject/constants': 'constants',
+                    '@ethersproject/bignumber': 'bignumber',
+                    '@ethersproject/address': 'address',
+                    '@ethersproject/bytes': 'bytes',
+                    '@ethersproject/abstract-signer': 'abstractSigner',
+                    '@ethersproject/contracts': 'contracts',
+                    '@balancer-labs/sor': 'sor',
+                    '@balancer-labs/typechain': 'typechain',
+                    '@ethersproject/providers': 'providers',
+                    'graphql-request': 'graphqlRequest',
+                    'json-to-graphql-query': 'jsonToGraphqlQuery',
+                    graphql: 'graphql',
+                    lodash: 'lodash',
+                    axios: 'axios',
+                },
+            },
+            { file: pkg.main, format: 'cjs', sourcemap: true },
+            { file: pkg.module, format: 'es', sourcemap: true },
+        ],
+        plugins: [
+            nodeResolve(),
+            json(),
+            commonjs(),
+            typescript({
+                exclude: ['node_modules', '**/*.spec.ts'],
+            }),
+            terser({
+                format: {
+                    comments: false,
+                },
+                compress: {
+                    pure_funcs: ['console.log', 'console.time', 'console.timeEnd'],
+                },
+            }),
+        ],
+        external,
+    },
+    {
+        input: 'src/index.ts',
+        output: [{ file: 'dist/index.d.ts', format: 'es' }],
+        plugins: [dts(), typescript({ exclude: ['node_modules', '**/*.spec.ts'] })],
+    },
+];
