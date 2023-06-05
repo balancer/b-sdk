5d38d4fe243ba7ca9f4518e49f80b4de5e3b8c3a
Daniel <danielBeets@protonmail.com>
2023-06-05 11:00:27 +0800

    refactor the token configs

--- src/schema/pool-schema.ts
+++ src/schema/pool-schema.ts
@@ -29,7 +29,7 @@ enum BalancerPoolOwnerType {
   EXTERNAL = "EXTERNAL",
 }
 
-enum BalancerJoinType {
+enum BalancerPoolJoinType {
   PROPORTIONAL = "PROPORTIONAL",
   SINGLE_ASSET = "SINGLE_ASSET",
   ANY_TOKENS = "ANY_TOKENS",
@@ -121,56 +121,85 @@ interface BalancerPoolDataUSD {
 }
 
 interface BalancerPoolJoinConfig {
-  supportedTypes: BalancerJoinType[];
+  supportedTypes: BalancerPoolJoinType[];
 
-  poolTokenConfigs: {
-    poolTokenIndex: number;
-    poolTokenAddress: Address;
-    // Leaf tokens are tokens that represent the last node in the graph. These are most often the
-    // "normal" tokens that users hold in their wallet ie: USDC/USDT/DAI instead of bbausd
-    // We consider the main token of a linear pool the leaf token.
-    leafTokens: Address[];
-    nestedStructure: (
-      | BalancerPoolNodeStableBpt
-      | BalancerPoolNodeLinearBpt
-      | BalancerPoolNodeStandardToken
-    )[];
-  }[];
+  // configs for each pool token
+  poolTokenConfigs: BalancerPoolTokenJoinConfig[];
 
   // the ui doesn't use proportional joins, it uses EXACT_TOKENS_IN_FOR_BPT_OUT to allow the
-  // user to configure any amount for each pool token, but additionally provides a proportional estimation.
-  // This concept could maybe use a better name.
+  // user to configure any amount for each pool token, this concept could maybe use a better name.
+  // Additionally, the ui provides a proportional estimation when applicable.
+  //
   // Since it's possible not all tokens are being used for a given join, this path is not always the
   // "most efficient" for a given combination of tokens, but for the sake of limiting the scope, we
   // provide a "generic" path that works for any given combination.
   steps: BalancerPoolJoinStep[];
+
+  // Leaf tokens are tokens that represent the last node in the tree. These are most often the
+  // "normal" tokens that users hold in their wallet ie: USDC/USDT/DAI instead of bbausd
+  // We consider the main token of a linear pool the leaf token.
+  leafTokens: Address[];
 }
 
-interface BalancerPoolGraphBaseToken {
+interface BalancerPoolTokenJoinConfig {
+  poolTokenIndex: number;
+  poolTokenAddress: Address;
+  // To scope this, we support the following options:
+  // - Standard token (the pool token is not a nested bpt)
+  // - Linear pool main token (the pool token is a nested linear bpt)
+  // - Stable BPT (the pool token is a nested stable bpt)
+  // - Leaf tokens of a nested stable BPT (the pool token is a nested stable bpt)
+  options: BalancerPoolTokenJoinConfigOptions[];
+}
+
+type BalancerPoolTokenJoinConfigOptions =
+  | BalancerPoolTokenConfigStandardToken
+  | BalancerPoolTokenConfigLinearMainToken
+  | BalancerPoolTokenConfigStableBpt
+  | BalancerPoolTokenConfigStableBptLeafTokens;
+
+interface BalancerPoolConfigBaseToken {
   address: Address;
 }
 
-interface BalancerPoolNodeStandardToken extends BalancerPoolGraphBaseToken {
-  __typename: "BalancerPoolNodeStandardToken";
+interface BalancerPoolTokenConfigStandardToken
+  extends BalancerPoolConfigBaseToken {
+  __typename: "BalancerPoolTokenConfigStandardToken";
 }
 
-interface BalancerPoolNodeLinearBpt extends BalancerPoolGraphBaseToken {
-  __typename: "BalancerPoolNodeLinearBpt";
+interface BalancerPoolConfigLinearBpt extends BalancerPoolConfigBaseToken {
+  __typename: "BalancerPoolConfigLinearBpt";
   poolId: ID;
-  //TODO: may be better to use an array of children here, would see on implementation if this is too verbose
-  mainToken: BalancerPoolNodeStandardToken;
-  wrappedToken: BalancerPoolNodeStandardToken;
 }
 
-interface BalancerPoolNodeStableBpt extends BalancerPoolGraphBaseToken {
-  __typename: "BalancerPoolNodeStableBpt";
+interface BalancerPoolTokenConfigLinearMainToken
+  extends BalancerPoolConfigBaseToken {
+  __typename: "BalancerPoolTokenConfigLinearMainToken";
   poolId: ID;
-  children: (BalancerPoolNodeStandardToken | BalancerPoolNodeLinearBpt)[];
+}
+
+interface BalancerPoolConfigLinearWrappedToken
+  extends BalancerPoolConfigBaseToken {
+  __typename: "BalancerPoolConfigLinearWrappedToken";
+  poolId: ID;
+}
+
+interface BalancerPoolTokenConfigStableBpt extends BalancerPoolConfigBaseToken {
+  __typename: "BalancerPoolConfigStableBpt";
+  poolId: ID;
+}
+
+interface BalancerPoolTokenConfigStableBptLeafTokens
+  extends BalancerPoolConfigBaseToken {
+  __typename: "BalancerPoolTokenConfigStableBptLeafTokens";
+  poolId: ID;
+  // the leaf tokens of this stable bpt
+  leafTokens: Address[];
 }
 
 export type BalancerPoolJoinStep =
   | BalancerPoolJoinBatchSwapStep
-  | PoolJoinMintBptStep;
+  | BalancerPoolJoinMintBptStep;
 
 // Currently, there should only ever be one batchSwap step that swaps any linear main tokens for the
 // corresponding linear phantom BPT
@@ -187,8 +216,8 @@ export interface BalancerPoolJoinBatchSwapStep {
 
 // The mint BPT step is a standard join step. At least one mint step will always occur on the pool itself.
 // Additionally, a second mint step will be present if there is a nested stable BPT in the pool.
-export interface PoolJoinMintBptStep {
-  __typename: "PoolJoinMintBptStep";
+export interface BalancerPoolJoinMintBptStep {
+  __typename: "BalancerPoolJoinMintBptStep";
   poolId: ID;
   tokensIn: Address[];
 }
