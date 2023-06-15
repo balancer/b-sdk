55baf34a179125bb933087a8041dc711f35c3170
Bruno Eidam Guerios <bruno@balancerlabs.dev>
2023-06-15 10:26:07 -0300

    Fix jest matcher not working as expected

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,20 +1,28 @@
-expect.extend({
-    toBeCloseToDelta(received, expected, delta) {
-        if (
-            typeof received !== 'bigint' ||
-            typeof expected !== 'bigint' ||
-            typeof delta !== 'bigint'
-        ) {
-            throw new Error('toBeCloseToDelta expects BigInts');
+declare global {
+    namespace jest {
+        interface Matchers<R, T> {
+            toBeCloseToDelta(value: bigint, delta: bigint): R;
         }
+    }
+}
 
-        const receivedDelta =
-            received > expected ? received - expected : expected - received;
-
-        return {
-            message: () =>
-                `Expected ${expected} \nReceived ${received} \nExpected delta ${delta} \nReceived delta ${receivedDelta} \n`,
-            pass: receivedDelta <= delta,
-        };
+expect.extend({
+    toBeCloseToDelta(received, expected, delta): jest.CustomMatcherResult {
+        const pass = Math.abs(Number(received - expected)) <= Number(delta);
+        if (pass) {
+            return {
+                message: () =>
+                    `expected ${received} not to be within ${delta} of ${expected}`,
+                pass: true,
+            };
+        } else {
+            return {
+                message: () =>
+                    `expected ${received} to be within ${delta} of ${expected}`,
+                pass: false,
+            };
+        }
     },
 });
+
+export {};
