e1b71c7ab1af58837bffd45b8ecf3c53c9b40dd2
Luiz Gustavo Abou Hatem De Liz <luizgustavo@balancerlabs.dev>
2023-10-22 20:05:10 -0300

    Changing "chainId" type to ChainId instead of number; clarifying query and variables parameters type on fetch function;

--- src/data/providers/balancer-api/client/index.ts
+++ src/data/providers/balancer-api/client/index.ts
@@ -1,22 +1,31 @@
-export class BalancerApiClient {
-  apiUrl: string;
-  chainId: number;
-  constructor(apiUrl: string, chainId: number) {
-    this.apiUrl = apiUrl;
-    this.chainId = chainId;
-  }
+import { ChainId } from '../../../../utils';
 
-  async fetch(operationName: string, query: any, variables: any) {
-    const requestQuery = {
-      operationName,
-      query,
-      variables,
-    };
-    const response = await fetch(this.apiUrl, {
-      method: 'post',
-      body: JSON.stringify(requestQuery),
-      headers: { 'Content-Type': 'application/json', ChainId: this.chainId.toString() },
-    });
-    return response.json();
-  }
+export class BalancerApiClient {
+    apiUrl: string;
+    chainId: ChainId;
+    constructor(apiUrl: string, chainId: number) {
+        this.apiUrl = apiUrl;
+        this.chainId = chainId;
+    }
+
+    async fetch(
+        operationName: string,
+        query: string,
+        variables: { id: string },
+    ) {
+        const requestQuery = {
+            operationName,
+            query,
+            variables,
+        };
+        const response = await fetch(this.apiUrl, {
+            method: 'post',
+            body: JSON.stringify(requestQuery),
+            headers: {
+                'Content-Type': 'application/json',
+                ChainId: this.chainId.toString(),
+            },
+        });
+        return response.json();
+    }
 }
