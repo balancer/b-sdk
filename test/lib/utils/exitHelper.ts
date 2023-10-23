32541923e66e8d6b16219e5f0b41f2809ba25cec
Luiz Gustavo Abou Hatem De Liz <luizgustavo@balancerlabs.dev>
2023-10-22 23:21:36 -0300

    Adding comments for the doExit test helper function

--- test/lib/utils/exitHelper.ts
+++ test/lib/utils/exitHelper.ts
@@ -3,6 +3,18 @@ import { replaceWrapped, Token } from '../../../src';
 import { sendTransactionGetBalances } from './helper';
 import { expect } from 'vitest';
 
+/**
+ * Helper function that sends a exit transaction and check for balance deltas
+ * @param txInput
+ *     @param poolExit: PoolExit - The pool exit class, used to query the exit and build the exit call
+ *     @param poolInput: PoolStateInput - The state of the pool being exited
+ *     @param exitInput: ExitInput - The parameters of the exit transaction, example: bptIn, amountsOut, etc.
+ *     @param testAddress: Address - The address to send the transaction from
+ *     @param client: Client & PublicActions & WalletActions - The RPC client
+ *     @param slippage: Slippage - The slippage tolerance for the exit transaction
+ *     @param checkNativeBalance: boolean - Whether to check the native asset balance or not
+ *     @param chainId: ChainId - The Chain Id of the pool being exited, for example: 1 for Mainnet, 137 for Polygon, etc.
+ */
 export const doExit = async (txInput: ExitTxInput) => {
     const {
         poolExit,
@@ -36,8 +48,17 @@ export const doExit = async (txInput: ExitTxInput) => {
 
     let tokensForBalanceCheck;
     if (bptIndex < 0) {
+        /**
+         * If the pool type tokens list does not contains BPT (for example: Weighted), the poolTokensAddr
+         * will not have the bpt address, so we need to add it, so the balance delta of the bpt will be
+         * checked in the tests.
+         */
         tokensForBalanceCheck = [...poolTokensAddr, poolInput.address];
     } else {
+        /*
+          * If the pool type tokens list contains BPT (for example: Composable Stable), the poolTokensAddr
+          * will already have the bpt address, so we don't need to add it again.
+         */
         tokensForBalanceCheck = [...poolTokensAddr];
     }
 
@@ -56,11 +77,20 @@ export const doExit = async (txInput: ExitTxInput) => {
     let expectedDeltas;
 
     if (bptIndex < 0) {
+        /*
+          * If the pool type tokens list does not contains BPT (for example: Weighted), the queryResult.amountsOut
+          * does not include any amount out of bpt, so we just need to add the bptIn.amount to the expectedDeltas.
+         */
         expectedDeltas = [
             ...queryResult.amountsOut.map((a) => a.amount),
             queryResult.bptIn.amount,
         ];
     } else {
+        /*
+         * If the pool type tokens list contains BPT (for example: Composable Stable), the queryResult.amountsOut 
+         * includes the amount out of the bpt, which is always 0, so we need to remove it from the 
+         * expectedDeltas and put the bptIn.amount in its place.
+        */
         expectedDeltas = [
             ...queryResult.amountsOut.slice(0, bptIndex).map((a) => a.amount),
             queryResult.bptIn.amount,
