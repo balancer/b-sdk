fbb917a3188be64e4c569e26712bae6e717138a2
johngrantuk <johngrantuk@googlemail.com>
2023-11-09 10:45:19 +0000

    fix: We are working on assumption that API will return BPT token in token list (as current SG does) It does not currently do this so we have to add it manually

--- src/data/providers/balancer-api/modules/pool-state/index.ts
+++ src/data/providers/balancer-api/modules/pool-state/index.ts
@@ -78,14 +78,33 @@ export class Pools {
     constructor(private readonly balancerApiClient: BalancerApiClient) {}
 
     async fetchPoolState(id: string): Promise<PoolStateInput> {
-        const {
-            data: { poolGetPool },
-        } = await this.balancerApiClient.fetch({
+        const { data } = await this.balancerApiClient.fetch({
             query: this.poolStateQuery,
             variables: {
                 id,
             },
         });
+        const poolGetPool: PoolStateInput = data.poolGetPool;
+        /**
+         * TODO:
+         * We are working on assumption that API will return BPT token in token list (as current SG does)
+         * It does not currently do this so we have to add it manually
+         */
+        if (poolGetPool.type === 'PHANTOM_STABLE') {
+            let missingBPTIndex = 0;
+            const sortedIndexes = poolGetPool.tokens.map((t) => t.index).sort();
+            for (let i = 0; i < poolGetPool.tokens.length + 1; i++) {
+                if (i === poolGetPool.tokens.length || sortedIndexes[i] !== i) {
+                    missingBPTIndex = i;
+                    break;
+                }
+            }
+            poolGetPool.tokens.splice(missingBPTIndex, 0, {
+                index: missingBPTIndex,
+                address: poolGetPool.address,
+                decimals: 18,
+            });
+        }
         return poolGetPool;
     }
 }
