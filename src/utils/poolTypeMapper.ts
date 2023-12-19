19214353f543ee0451136bf8e9266fa90d59904f
Bruno Eidam Guerios <bruno@balancerlabs.dev>
2023-12-19 11:09:14 -0300

    Update api pool type from PHANTOM_STABLE to COMPOSABLE_STABLE

--- src/utils/poolTypeMapper.ts
+++ src/utils/poolTypeMapper.ts
@@ -2,7 +2,7 @@ import { PoolType } from '../types';
 
 export const poolTypeFromApi = {
     WEIGHTED: PoolType.Weighted,
-    PHANTOM_STABLE: PoolType.ComposableStable,
+    COMPOSABLE_STABLE: PoolType.ComposableStable,
     GYRO3: PoolType.Gyro3,
     GYRO2: PoolType.Gyro2,
     GYROE: PoolType.GyroE,
