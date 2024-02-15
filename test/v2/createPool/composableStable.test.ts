96d353502d5b161ef2aa1ce612aea96a0fbf43cb
Luiz Gustavo Abou Hatem De Liz <luizgustavo@balancerlabs.dev>
2024-02-15 18:41:34 -0300

    Moving unit tests from create pool to a different file from integrations;

--- test/v2/createPool/composableStable.test.ts
+++ test/v2/createPool/composableStable.test.ts
@@ -15,33 +15,14 @@ import { doCreatePool } from '../../lib/utils/createPoolHelper';
 import { CreatePoolTxInput } from '../../lib/utils/types';
 import {
     CreatePoolV2ComposableStableInput,
-    CreatePoolInput,
 } from '../../../src/entities/createPool/types';
-
-const { rpcUrl } = await startFork(ANVIL_NETWORKS.MAINNET);
 
 describe('Create Composable Stable Pool tests', () => {
     const chainId = ChainId.MAINNET;
     let txInput: CreatePoolTxInput;
-    let poolAddress: Address;
     let createPoolComposableStableInput: CreatePoolV2ComposableStableInput;
+    const createPool = new CreatePool();
     beforeAll(async () => {
-        const client = createTestClient({
-            mode: 'anvil',
-            chain: CHAINS[chainId],
-            transport: http(rpcUrl),
-        })
-            .extend(publicActions)
-            .extend(walletActions);
-        const signerAddress = (await client.getAddresses())[0];
-
-        txInput = {
-            client,
-            createPool: new CreatePool(),
-            testAddress: signerAddress,
-            createPoolInput: {} as CreatePoolInput,
-        };
-
         createPoolComposableStableInput = {
             name: 'Test Pool',
             poolType: PoolType.ComposableStable,
@@ -66,16 +47,39 @@ describe('Create Composable Stable Pool tests', () => {
             amplificationParameter: BigInt(67),
             exemptFromYieldProtocolFeeFlag: false,
             swapFee: '0.01',
-            poolOwnerAddress: txInput.testAddress, // Balancer DAO Multisig
+            poolOwnerAddress: zeroAddress,
             chainId,
             balancerVersion: 2,
         };
     });
-    test('Create Composable Stable Pool', async () => {
-        poolAddress = await doCreatePool({
-            ...txInput,
-            createPoolInput: createPoolComposableStableInput,
-        });
-        expect(poolAddress).to.not.be.undefined;
+    test('Amplification Parameter 0, expects error', async () => {
+        const amplificationParameter = BigInt(0);
+        expect(() =>
+            createPool.buildCall({...createPoolComposableStableInput, amplificationParameter})
+        ).toThrowError(
+            'Amplification parameter must be greater than 0',
+        );
+    });
+    test('Duplicate token addresses, expects error', async () => {
+        const tokens: CreatePoolV2ComposableStableInput['tokens'] = [
+            {
+                address: '0xba100000625a3754423978a60c9317c58a424e3d',
+                rateProvider: zeroAddress,
+                tokenRateCacheDuration: BigInt(100),
+            },
+            {
+                address: '0xba100000625a3754423978a60c9317c58a424e3d',
+                rateProvider: zeroAddress,
+                tokenRateCacheDuration: BigInt(100),
+            },
+            {
+                address: '0x6b175474e89094c44da98b954eedeac495271d0f',
+                rateProvider: zeroAddress,
+                tokenRateCacheDuration: BigInt(100),
+            },
+        ];
+        expect(() =>
+        createPool.buildCall({...createPoolComposableStableInput, tokens})
+    ).toThrowError('Duplicate token addresses');
     });
 });
