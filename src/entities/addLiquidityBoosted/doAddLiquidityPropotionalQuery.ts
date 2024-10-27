6252354a3820f08aa4345831bef5f4c2f79296ae
mkflow27 <mkflow27@gmail.com>
2024-10-27 10:18:22 +0100

    undo mock data return

--- src/entities/addLiquidityBoosted/doAddLiquidityPropotionalQuery.ts
+++ src/entities/addLiquidityBoosted/doAddLiquidityPropotionalQuery.ts
@@ -16,21 +16,16 @@ export const doAddLiquidityProportionalQuery = async (
         chain: CHAINS[chainId],
     });
 
-    try {
-        const { result: exactAmountsIn } = await client.simulateContract({
-            address: BALANCER_COMPOSITE_LIQUIDITY_ROUTER[chainId],
-            abi: balancerCompositeLiquidityRouterAbi,
-            functionName: 'queryAddLiquidityProportionalToERC4626Pool',
-            args: [
-                poolAddress,
-                referenceAmount.rawAmount,
-                getAddress('0xa5cc3c03994db5b0d9a5eEdD10Cabab0813678ac'),
-                '0x',
-            ],
-        });
-
-        return [...exactAmountsIn];
-    } catch (_e) {
-        return [1236n, 1234n];
-    }
+    const { result: exactAmountsIn } = await client.simulateContract({
+        address: BALANCER_COMPOSITE_LIQUIDITY_ROUTER[chainId],
+        abi: balancerCompositeLiquidityRouterAbi,
+        functionName: 'queryAddLiquidityProportionalToERC4626Pool',
+        args: [
+            poolAddress,
+            referenceAmount.rawAmount,
+            getAddress('0xa5cc3c03994db5b0d9a5eEdD10Cabab0813678ac'),
+            '0x',
+        ],
+    });
+    return [...exactAmountsIn];
 };
--- src/entities/addLiquidityBoosted/doAddLiquidityUnbalancedQuery.ts
+++ src/entities/addLiquidityBoosted/doAddLiquidityUnbalancedQuery.ts
@@ -16,20 +16,16 @@ export const doAddLiquidityUnbalancedQuery = async (
         chain: CHAINS[chainId],
     });
 
-    try {
-        const { result: bptAmountOut } = await client.simulateContract({
-            address: BALANCER_COMPOSITE_LIQUIDITY_ROUTER[chainId],
-            abi: balancerCompositeLiquidityRouterAbi,
-            functionName: 'queryAddLiquidityUnbalancedToERC4626Pool',
-            args: [
-                poolAddress,
-                amountsIn.map((input) => input.rawAmount),
-                getAddress('0xa5cc3c03994db5b0d9a5eEdD10Cabab0813678ac'),
-                '0x',
-            ],
-        });
-        return bptAmountOut;
-    } catch (_e) {
-        return 1999413483897830545n;
-    }
+    const { result: bptAmountOut } = await client.simulateContract({
+        address: BALANCER_COMPOSITE_LIQUIDITY_ROUTER[chainId],
+        abi: balancerCompositeLiquidityRouterAbi,
+        functionName: 'queryAddLiquidityUnbalancedToERC4626Pool',
+        args: [
+            poolAddress,
+            amountsIn.map((input) => input.rawAmount),
+            getAddress('0xa5cc3c03994db5b0d9a5eEdD10Cabab0813678ac'),
+            '0x',
+        ],
+    });
+    return bptAmountOut;
 };
