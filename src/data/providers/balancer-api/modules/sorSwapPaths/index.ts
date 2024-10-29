185caf28f6c98a5c2797c9d1f125ed9678dde637
bearpong <178402093+bearpong@users.noreply.github.com>
2024-10-29 18:44:44 +0100

    refactor: renamed package name

--- src/data/providers/balancer-api/modules/sorSwapPaths/index.ts
+++ src/data/providers/balancer-api/modules/sorSwapPaths/index.ts
@@ -1,21 +1,22 @@
-import { BalancerApiClient } from '../../client';
-import { API_CHAIN_NAMES, ChainId } from '@/utils';
-import { Address } from 'viem';
-import { SwapKind } from '@/types';
-import { TokenAmount } from '@/entities';
-import { Path } from '@/entities/swap/paths/types';
+import { TokenAmount } from "@/entities";
+import { Path } from "@/entities/swap/paths/types";
+import { SwapKind } from "@/types";
+import { API_CHAIN_NAMES, ChainId } from "@/utils";
+import { Address } from "viem";
+
+import { BalancerApiClient } from "../../client";
 
 export type SorInput = {
-    chainId: ChainId;
-    tokenIn: Address;
-    tokenOut: Address;
-    swapKind: SwapKind;
-    swapAmount: TokenAmount; // API expects input in human readable form
-    useProtocolVersion?: 2 | 3; // If not specified API will return best
+  chainId: ChainId;
+  tokenIn: Address;
+  tokenOut: Address;
+  swapKind: SwapKind;
+  swapAmount: TokenAmount; // API expects input in human readable form
+  useProtocolVersion?: 2 | 3; // If not specified API will return best
 };
 
 export class SorSwapPaths {
-    readonly sorSwapPathQuery = `#graphql
+  readonly sorSwapPathQuery = `#graphql
   query MyQuery($chain: GqlChain!, $swapType: GqlSorSwapType!, $swapAmount: AmountHumanReadable!, $tokenIn: String!, $tokenOut: String!) {
     sorGetSwapPaths(
     swapAmount: $swapAmount
@@ -40,13 +41,12 @@ export class SorSwapPaths {
         tokens {
           address
           decimals
-          
         }
       }
     }
   }
 `;
-    readonly sorSwapPathQueryWithVersion = `
+  readonly sorSwapPathQueryWithVersion = `#graphql
   query MyQuery($chain: GqlChain!, $swapType: GqlSorSwapType!, $swapAmount: AmountHumanReadable!, $tokenIn: String!, $tokenOut: String!, $useProtocolVersion: Int!) {
     sorGetSwapPaths(
     swapAmount: $swapAmount
@@ -78,39 +78,37 @@ export class SorSwapPaths {
   }
 `;
 
-    constructor(private readonly balancerApiClient: BalancerApiClient) {}
+  constructor(private readonly balancerApiClient: BalancerApiClient) {}
 
-    async fetchSorSwapPaths(sorInput: SorInput): Promise<Path[]> {
-        const variables = {
-            chain: this.mapGqlChain(sorInput.chainId),
-            swapAmount: sorInput.swapAmount.toSignificant(
-                sorInput.swapAmount.token.decimals,
-            ), // Must use human scale
-            swapType:
-                sorInput.swapKind === SwapKind.GivenIn
-                    ? 'EXACT_IN'
-                    : 'EXACT_OUT',
-            tokenIn: sorInput.tokenIn,
-            tokenOut: sorInput.tokenOut,
-        };
-        const { data } = await this.balancerApiClient.fetch({
-            query: sorInput.useProtocolVersion
-                ? this.sorSwapPathQueryWithVersion
-                : this.sorSwapPathQuery,
-            variables: sorInput.useProtocolVersion
-                ? {
-                      ...variables,
-                      useProtocolVersion: sorInput.useProtocolVersion,
-                  }
-                : variables,
-        });
-        const poolGetPool: Path[] = data.sorGetSwapPaths.paths;
-        return poolGetPool;
-    }
+  async fetchSorSwapPaths(sorInput: SorInput): Promise<Path[]> {
+    const variables = {
+      chain: this.mapGqlChain(sorInput.chainId),
+      swapAmount: sorInput.swapAmount.toSignificant(
+        sorInput.swapAmount.token.decimals,
+      ), // Must use human scale
+      swapType:
+        sorInput.swapKind === SwapKind.GivenIn ? "EXACT_IN" : "EXACT_OUT",
+      tokenIn: sorInput.tokenIn,
+      tokenOut: sorInput.tokenOut,
+    };
+    const { data } = await this.balancerApiClient.fetch({
+      query: sorInput.useProtocolVersion
+        ? this.sorSwapPathQueryWithVersion
+        : this.sorSwapPathQuery,
+      variables: sorInput.useProtocolVersion
+        ? {
+            ...variables,
+            useProtocolVersion: sorInput.useProtocolVersion,
+          }
+        : variables,
+    });
+    const poolGetPool: Path[] = data.sorGetSwapPaths.paths;
+    return poolGetPool;
+  }
 
-    private mapGqlChain(chainId: ChainId): string {
-        if (chainId in API_CHAIN_NAMES) {
-            return API_CHAIN_NAMES[chainId];
-        } else throw Error(`Unsupported API chain: ${chainId}`);
-    }
+  private mapGqlChain(chainId: ChainId): string {
+    if (chainId in API_CHAIN_NAMES) {
+      return API_CHAIN_NAMES[chainId];
+    } else throw Error(`Unsupported API chain: ${chainId}`);
+  }
 }
