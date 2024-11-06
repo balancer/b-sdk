e09c10a159922356d6cbd5827fdd3923c6dade42
mkflow27 <mkflow27@gmail.com>
2024-11-06 19:10:58 +0100

    test: change numbers

--- test/v3/priceImpact/priceImpact.V3.integration.test.ts
+++ test/v3/priceImpact/priceImpact.V3.integration.test.ts
@@ -110,7 +110,7 @@ describe('PriceImpact V3', () => {
                     addLiquidityInput,
                     boostedPool_USDC_USDT,
                 );
-            const priceImpactSpot = PriceImpactAmount.fromDecimal('0.0004755');
+            const priceImpactSpot = PriceImpactAmount.fromDecimal('0.000476');
             expect(priceImpactABA.decimal).eq(priceImpactSpot.decimal);
         });
     });
@@ -140,7 +140,7 @@ describe('PriceImpact V3', () => {
                     addLiquidityInput,
                     partialBoostedPool_USDT_stataDAI,
                 );
-            const priceImpactSpot = PriceImpactAmount.fromDecimal('0.0000065');
+            const priceImpactSpot = PriceImpactAmount.fromDecimal('0.000016');
             expect(priceImpactABA.decimal).eq(priceImpactSpot.decimal);
         });
 
@@ -168,7 +168,7 @@ describe('PriceImpact V3', () => {
                     addLiquidityInput,
                     partialBoostedPool_USDT_stataDAI,
                 );
-            const priceImpactSpot = PriceImpactAmount.fromDecimal('0.0056435');
+            const priceImpactSpot = PriceImpactAmount.fromDecimal('0.00566425');
             expect(priceImpactABA.decimal).eq(priceImpactSpot.decimal);
         });
     });
