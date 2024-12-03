cc3053c40b5055469a724a398b14f25824fa722a
Bruno Eidam Guerios <bruno@balancerlabs.dev>
2024-12-03 10:14:44 -0300

    Add native variations to AddLiquidityNested integration tests

--- test/v3/addLiquidityNested/addLiquidityNestedV3.integration.test.ts
+++ test/v3/addLiquidityNested/addLiquidityNestedV3.integration.test.ts
@@ -4,11 +4,13 @@ dotenv.config();
 
 import {
     createTestClient,
+    Hex,
     http,
     parseUnits,
     publicActions,
     TestActions,
     walletActions,
+    zeroAddress,
 } from 'viem';
 import {
     Address,
@@ -23,6 +25,7 @@ import {
     AddLiquidityNested,
     AddLiquidityNestedInput,
     AddLiquidityNestedQueryOutputV3,
+    AddLiquidityNestedCallInput,
 } from '@/index';
 import { ANVIL_NETWORKS, startFork } from 'test/anvil/anvil-global-setup';
 import {
@@ -31,6 +34,7 @@ import {
     areBigIntsWithinPercent,
     sendTransactionGetBalances,
     setTokenBalances,
+    TxOutput,
 } from 'test/lib/utils';
 import {
     nestedWithBoostedPool,
@@ -57,6 +61,7 @@ describe('V3 add liquidity nested test, with Permit2 direct approval', () => {
     let rpcUrl: string;
     let client: PublicWalletClient & TestActions;
     let testAddress: Address;
+    let snapshot: Hex;
     const addLiquidityNested = new AddLiquidityNested();
 
     beforeAll(async () => {
@@ -80,6 +85,14 @@ describe('V3 add liquidity nested test, with Permit2 direct approval', () => {
             [WETH.slot, USDT.slot, USDC.slot] as number[],
             mainTokens.map((t) => parseUnits('1000', t.decimals)),
         );
+        snapshot = await client.snapshot();
+    });
+
+    beforeEach(async () => {
+        await client.revert({
+            id: snapshot,
+        });
+        snapshot = await client.snapshot();
     });
 
     test('query with underlying', async () => {
@@ -114,89 +127,183 @@ describe('V3 add liquidity nested test, with Permit2 direct approval', () => {
         expect(queryOutput.amountsIn).to.deep.eq(expectedAmountsIn);
     });
 
-    test('add liquidity transaction', async () => {
-        const addLiquidityInput: AddLiquidityNestedInput = {
-            amountsIn: [
-                {
-                    address: WETH.address,
-                    rawAmount: parseUnits('0.001', WETH.decimals),
-                    decimals: WETH.decimals,
-                },
-                {
-                    address: USDC.address,
-                    rawAmount: parseUnits('2', USDC.decimals),
-                    decimals: USDC.decimals,
-                },
-            ],
-            chainId,
-            rpcUrl,
-        };
+    describe('add liquidity transaction', async () => {
+        test('with tokens', async () => {
+            const addLiquidityInput: AddLiquidityNestedInput = {
+                amountsIn: [
+                    {
+                        address: WETH.address,
+                        rawAmount: parseUnits('0.001', WETH.decimals),
+                        decimals: WETH.decimals,
+                    },
+                    {
+                        address: USDC.address,
+                        rawAmount: parseUnits('2', USDC.decimals),
+                        decimals: USDC.decimals,
+                    },
+                ],
+                chainId,
+                rpcUrl,
+            };
 
-        for (const amount of addLiquidityInput.amountsIn) {
-            // Approve Permit2 to spend account tokens
-            await approveSpenderOnToken(
-                client,
-                testAddress,
-                amount.address,
-                PERMIT2[chainId],
+            for (const amount of addLiquidityInput.amountsIn) {
+                // Approve Permit2 to spend account tokens
+                await approveSpenderOnToken(
+                    client,
+                    testAddress,
+                    amount.address,
+                    PERMIT2[chainId],
+                );
+                // Approve Router to spend account tokens using Permit2
+                await approveSpenderOnPermit2(
+                    client,
+                    testAddress,
+                    amount.address,
+                    BALANCER_COMPOSITE_LIQUIDITY_ROUTER[chainId],
+                );
+            }
+
+            const queryOutput = (await addLiquidityNested.query(
+                addLiquidityInput,
+                nestedWithBoostedPool,
+            )) as AddLiquidityNestedQueryOutputV3;
+
+            const addLiquidityBuildInput = {
+                ...queryOutput,
+                slippage: Slippage.fromPercentage('1'), // 1%,
+            };
+
+            const addLiquidityBuildCallOutput = addLiquidityNested.buildCall(
+                addLiquidityBuildInput,
             );
-            // Approve Router to spend account tokens using Permit2
-            await approveSpenderOnPermit2(
-                client,
-                testAddress,
-                amount.address,
+            expect(addLiquidityBuildCallOutput.value === 0n).to.be.true;
+            expect(addLiquidityBuildCallOutput.to).to.eq(
                 BALANCER_COMPOSITE_LIQUIDITY_ROUTER[chainId],
             );
-        }
 
-        const queryOutput = (await addLiquidityNested.query(
-            addLiquidityInput,
-            nestedWithBoostedPool,
-        )) as AddLiquidityNestedQueryOutputV3;
+            // send add liquidity transaction and check balance changes
+            const { transactionReceipt, balanceDeltas } =
+                await sendTransactionGetBalances(
+                    [
+                        ...mainTokens.map((t) => t.address),
+                        queryOutput.bptOut.token.address,
+                    ],
+                    client,
+                    testAddress,
+                    addLiquidityBuildCallOutput.to,
+                    addLiquidityBuildCallOutput.callData,
+                    addLiquidityBuildCallOutput.value,
+                );
 
-        const addLiquidityBuildInput = {
-            ...queryOutput,
-            slippage: Slippage.fromPercentage('1'), // 1%,
-        };
+            expect(transactionReceipt.status).to.eq('success');
+            const expectedAmountsIn = [
+                TokenAmount.fromHumanAmount(wethToken, '0.001'),
+                TokenAmount.fromHumanAmount(usdtToken, '0'),
+                TokenAmount.fromHumanAmount(usdcToken, '2'),
+            ];
 
-        const addLiquidityBuildCallOutput = addLiquidityNested.buildCall(
-            addLiquidityBuildInput,
-        );
-        expect(addLiquidityBuildCallOutput.value === 0n).to.be.true;
-        expect(addLiquidityBuildCallOutput.to).to.eq(
-            BALANCER_COMPOSITE_LIQUIDITY_ROUTER[chainId],
-        );
+            expect(expectedAmountsIn.map((a) => a.amount)).to.deep.eq(
+                balanceDeltas.slice(0, -1),
+            );
+            // Here we check that output diff is within an acceptable tolerance.
+            // !!! This should only be used in the case of buffers as all other cases can be equal
+            areBigIntsWithinPercent(
+                balanceDeltas[balanceDeltas.length - 1],
+                queryOutput.bptOut.amount,
+                0.001,
+            );
+        });
 
-        // send add liquidity transaction and check balance changes
-        const { transactionReceipt, balanceDeltas } =
-            await sendTransactionGetBalances(
-                [
-                    ...mainTokens.map((t) => t.address),
-                    queryOutput.bptOut.token.address,
+        test('with native', async () => {
+            const addLiquidityInput: AddLiquidityNestedInput = {
+                amountsIn: [
+                    {
+                        address: WETH.address,
+                        rawAmount: parseUnits('0.001', WETH.decimals),
+                        decimals: WETH.decimals,
+                    },
+                    {
+                        address: USDC.address,
+                        rawAmount: parseUnits('2', USDC.decimals),
+                        decimals: USDC.decimals,
+                    },
                 ],
-                client,
-                testAddress,
-                addLiquidityBuildCallOutput.to,
-                addLiquidityBuildCallOutput.callData,
-                addLiquidityBuildCallOutput.value,
+                chainId,
+                rpcUrl,
+            };
+
+            for (const amount of addLiquidityInput.amountsIn) {
+                // Approve Permit2 to spend account tokens
+                await approveSpenderOnToken(
+                    client,
+                    testAddress,
+                    amount.address,
+                    PERMIT2[chainId],
+                );
+                // Approve Router to spend account tokens using Permit2
+                await approveSpenderOnPermit2(
+                    client,
+                    testAddress,
+                    amount.address,
+                    BALANCER_COMPOSITE_LIQUIDITY_ROUTER[chainId],
+                );
+            }
+
+            const queryOutput = (await addLiquidityNested.query(
+                addLiquidityInput,
+                nestedWithBoostedPool,
+            )) as AddLiquidityNestedQueryOutputV3;
+
+            const addLiquidityBuildInput: AddLiquidityNestedCallInput = {
+                ...queryOutput,
+                slippage: Slippage.fromPercentage('1'), // 1%,
+                wethIsEth: true,
+            };
+
+            const addLiquidityBuildCallOutput = addLiquidityNested.buildCall(
+                addLiquidityBuildInput,
+            );
+            expect(
+                addLiquidityBuildCallOutput.value ===
+                    addLiquidityInput.amountsIn[0].rawAmount,
+            ).to.be.true;
+            expect(addLiquidityBuildCallOutput.to).to.eq(
+                BALANCER_COMPOSITE_LIQUIDITY_ROUTER[chainId],
             );
 
-        expect(transactionReceipt.status).to.eq('success');
-        const expectedAmountsIn = [
-            TokenAmount.fromHumanAmount(wethToken, '0.001'),
-            TokenAmount.fromHumanAmount(usdtToken, '0'),
-            TokenAmount.fromHumanAmount(usdcToken, '2'),
-        ];
+            // send add liquidity transaction and check balance changes
+            const { transactionReceipt, balanceDeltas } =
+                await sendTransactionGetBalances(
+                    [
+                        zeroAddress,
+                        ...mainTokens.map((t) => t.address),
+                        queryOutput.bptOut.token.address,
+                    ],
+                    client,
+                    testAddress,
+                    addLiquidityBuildCallOutput.to,
+                    addLiquidityBuildCallOutput.callData,
+                    addLiquidityBuildCallOutput.value,
+                );
 
-        expect(expectedAmountsIn.map((a) => a.amount)).to.deep.eq(
-            balanceDeltas.slice(0, -1),
-        );
-        // Here we check that output diff is within an acceptable tolerance.
-        // !!! This should only be used in the case of buffers as all other cases can be equal
-        areBigIntsWithinPercent(
-            balanceDeltas[balanceDeltas.length - 1],
-            queryOutput.bptOut.amount,
-            0.001,
-        );
+            expect(transactionReceipt.status).to.eq('success');
+            const expectedAmountsIn = [
+                TokenAmount.fromHumanAmount(wethToken, '0.001'), // checking against ETH
+                TokenAmount.fromHumanAmount(wethToken, '0'), // checking against WETH
+                TokenAmount.fromHumanAmount(usdtToken, '0'),
+                TokenAmount.fromHumanAmount(usdcToken, '2'),
+            ];
+
+            expect(expectedAmountsIn.map((a) => a.amount)).to.deep.eq(
+                balanceDeltas.slice(0, -1),
+            );
+            // Here we check that output diff is within an acceptable tolerance.
+            // !!! This should only be used in the case of buffers as all other cases can be equal
+            areBigIntsWithinPercent(
+                balanceDeltas[balanceDeltas.length - 1],
+                queryOutput.bptOut.amount,
+                0.001,
+            );
+        });
     });
 });
