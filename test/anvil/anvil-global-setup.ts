e96114019c9f32456027b5df0bf060c39044220a
Matt Pereira <mattpereira.dev@gmail.com>
2025-04-07 15:49:12 -0700

    Update test block number for new stable pool factory

--- test/anvil/anvil-global-setup.ts
+++ test/anvil/anvil-global-setup.ts
@@ -66,7 +66,7 @@ export const ANVIL_NETWORKS: Record<NetworksWithFork, NetworkSetup> = {
         rpcEnv: 'SEPOLIA_RPC_URL',
         fallBackRpc: 'https://sepolia.gateway.tenderly.co',
         port: ANVIL_PORTS.SEPOLIA,
-        forkBlockNumber: 7655008n,
+        forkBlockNumber: 8000000n,
     },
     OPTIMISM: {
         rpcEnv: 'OPTIMISM_RPC_URL',
