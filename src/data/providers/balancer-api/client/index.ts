6879394fb91e4e18fb0beff98703dd5dc756151e
gmbronco <83549293+gmbronco@users.noreply.github.com>
2025-05-09 13:51:43 +0200

    API client headers

--- src/data/providers/balancer-api/client/index.ts
+++ src/data/providers/balancer-api/client/index.ts
@@ -3,9 +3,15 @@ import { ChainId } from '../../../../utils';
 export class BalancerApiClient {
     apiUrl: string;
     chainId: ChainId;
+    clientName: string;
+    clientVersion: string;
+
     constructor(apiUrl: string, chainId: ChainId) {
         this.apiUrl = apiUrl;
         this.chainId = chainId;
+
+        this.clientName = process.env.API_CLIENT_NAME || 'balancer-sdk';
+        this.clientVersion = process.env.API_CLIENT_VERSION || __PACKAGE_VERSION__;
     }
 
     async fetch(requestQuery: {
@@ -19,6 +25,8 @@ export class BalancerApiClient {
             headers: {
                 'Content-Type': 'application/json',
                 ChainId: this.chainId.toString(),
+                'x-graphql-client-name': this.clientName,
+                'x-graphql-client-version': this.clientVersion,
             },
         });
         return response.json();
