2def150ee825ea23dc054cfb5aac43714fb72d53
johngrantuk <johngrantuk@googlemail.com>
2026-06-05 10:44:27 +0100

    Update codegen command for new version.

--- src/data/providers/balancer-api/generated/types.ts
+++ src/data/providers/balancer-api/generated/types.ts
@@ -1,10 +1,5 @@
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
-export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
-export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
-export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: { input: string; output: string; }
@@ -16,9 +11,9 @@ export type Scalars = {
   BigDecimal: { input: string; output: string; }
   BigInt: { input: string; output: string; }
   Bytes: { input: string; output: string; }
-  Date: { input: any; output: any; }
-  GqlBigNumber: { input: any; output: any; }
-  JSON: { input: any; output: any; }
+  Date: { input: unknown; output: unknown; }
+  GqlBigNumber: { input: unknown; output: unknown; }
+  JSON: { input: unknown; output: unknown; }
 };
 
 export type CreateLBPInput = {
